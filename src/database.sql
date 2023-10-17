51287e1fa01c0cd0bfe873bc432b04565e98b092
Artur <artur.alb@outlook.com>
2023-10-17 12:31:59 +0100

    fixed some issues in database.sql

--- src/database.sql
+++ src/database.sql
@@ -12,113 +12,97 @@ DROP TABLE IF EXISTS comment CASCADE;
 DROP TABLE IF EXISTS comment_like CASCADE;
 DROP TABLE IF EXISTS faq CASCADE;
 
-CREATE TABLE user {
-    id INT PRIMARY KEY,
+CREATE TABLE user(
+    id SERIAL PRIMARY KEY,
     username VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     email VARCHAR(50) UNIQUE,
-};
+);
 
-CREATE TABLE game_developer{
+CREATE TABLE game_developer(
     id SERIAL PRIMARY KEY,
     id_user INT NOT NULL UNIQUE,
-    FOREIGN KEY(id_user),
-    REFERENCES user(id)
-};
+    FOREIGN KEY(id_user) REFERENCES user(id)
+);
 
-CREATE TABLE admin{
+CREATE TABLE admin(
     id SERIAL PRIMARY KEY,
     id_user INT NOT NULL UNIQUE,
-    FOREIGH KEY(id_user),
-    REFERENCES user(id)
-};
+    FOREIGH KEY(id_user) REFERENCES user(id)
+);
 
-CREATE TABLE event{
+CREATE TABLE event(
     id SERIAL PRIMARY KEY,
     id_host INT NOT NULL,
     start DATE NOT NULL,
-    end DATE NOT NULL CHECK end > start,
+    end_ DATE NOT NULL CHECK (end_ > start),
     name TEXT NOT NULL,
-    description NOT NULL,
-    type NOT NULL,
-    FOREIGN KEY(id_host),
-    REFERENCES game_developer(id)
-};
+    description TEXT NOT NULL,
+    type CHECK type IN ('public', 'private', 'protected') NOT NULL,
+    FOREIGN KEY(id_host) REFERENCES game_developer(id)
+);
 
-CREATE TABLE participates{
+CREATE TABLE participates(
     id_participant INT NOT NULL,
     id_event INT NOT NULL,
-    FOREIGN KEY(id_developer),
-    REFERENCES game_developer(id),
-    FOREIGN KEY(id_event),
-    REFERENCES event(id)
-};
+    FOREIGN KEY(id_developer) REFERENCES game_developer(id),
+    FOREIGN KEY(id_event) REFERENCES event(id)
+);
 
-CREATE TABLE tag{
+CREATE TABLE tag(
     id SERIAL PRIMARY KEY;
     name VARCHAR(20) NOT NULL,
-};
+);
 
-CREATE TABLE event_tag{
-    id_event INT NOT NULL,
-    id_tag INT NOT NULL,
-    FOREIGN KEY(id_event),
-    REFERENCES event(id),
-    FOREIGN KEY(id_tag),
-    REFERENCES tag(id)
-};
+CREATE TABLE event_tag(
+    id_event INT,
+    id_tag INT,
+    FOREIGN KEY(id_event) REFERENCES event(id),
+    FOREIGN KEY(id_tag) REFERENCES tag(id)
+);
 
-CREATE TABLE poll{
+CREATE TABLE poll(
     id SERIAL PRIMARY KEY,
     id_event INT NOT NULL,
-    FOREIGN KEY(id_event),
-    REFERENCES event(id)
-};
+    FOREIGN KEY(id_event) REFERENCES event(id)
+);
 
-CREATE TABLE option{
+CREATE TABLE option(
     id SERIAL PRIMARY KEY,
     id_poll INT NOT NULL,
     name VARCHAR(20) NOT NULL,
-    FOREIGN KEY(id_poll),
-    REFERENCES poll(id)
-};
+    FOREIGN KEY(id_poll) REFERENCES poll(id)
+);
 
-CREATE TABLE developer_option{
-    id_developer INT NOT NULL,
-    id_option INT NOT NULL,
-    FOREIGN KEY(id_developer),
-    REFERENCES game_developer(id),
-    FOREIGN KEY(id_option),
-    REFERENCES option(id)
-};
+CREATE TABLE votes(
+    id_developer INT,
+    id_option INT,
+    FOREIGN KEY(id_developer) REFERENCES game_developer(id),
+    FOREIGN KEY(id_option) REFERENCES option(id)
+);
 
-CREATE TABLE comment{
+CREATE TABLE comment(
     id SERIAL PRIMARY KEY,
-    id_writer INT NOT NULL,
-    id_event INT NOT NULL,
+    id_writer INT,
+    id_event INT,
     content TEXT NOT NULL,
     time TIMESTAMP NOT NULL,
-    FOREIGN KEY(id_writer),
-    REFERENCES game_developer(id),
-    FOREIGN KEY(id_event),
-    REFERENCES event(id)
-};
+    FOREIGN KEY(id_writer) REFERENCES game_developer(id),
+    FOREIGN KEY(id_event) REFERENCES event(id)
+);
 
-CREATE TABLE comment_like{
-    id_comment INT NOT NULL,
-    id_developer INT NOT NULL,
+CREATE TABLE likes(
+    id_comment INT,
+    id_developer INT,
     like BOOLEAN NOT NULL,
-    FOREIGN KEY(id_comment),
-    REFERENCES comment(id),
-    FOREIGN KEY(id_developer),
-    REFERENCES game_developer(id)
-};
+    FOREIGN KEY(id_comment) REFERENCES comment(id),
+    FOREIGN KEY(id_developer) REFERENCES game_developer(id)
+);
 
-CREATE TABLE faq{
+CREATE TABLE faq(
     id SERIAL PRIMARY KEY,
     id_admin INT NOT NULL,
     question TEXT NOT NULL,
     answer TEXT NOT NULL,
-    FOREIGN KEY(id_admin),
-    REFERENCES admin(id)
-};
+    FOREIGN KEY(id_admin) REFERENCES admin(id)
+);
