1a034ab318d9852f86b7a8012a1876f86f922755
tomas-sucena <tomas21sucena@gmail.com>
2023-10-23 01:56:36 +0100

    Add script for populating the database

--- src/triggers.sql
+++ src/triggers.sql
@@ -1,12 +1,7 @@
-pragma foreign_keys = ON;
-.mode columns
-.header on
-.nullvalue null
-
 CREATE FUNCTION verify_participation_limit() RETURNS TRIGGER AS $BODY$
 BEGIN 
     IF EXISTS (SELECT ParticipantCount FROM (SELECT count(*) AS ParticipantCount FROM participants WHERE participants.id_event = NEW.id_event) WHERE ParticipantCount >= 100) THEN
-        RAISE EXCEPTION 'PARTICIPATION LIMIT REACHED!'
+        RAISE EXCEPTION 'PARTICIPATION LIMIT REACHED!';
     END IF;
     RETURN NEW;
 END $BODY$
@@ -17,12 +12,10 @@ CREATE TRIGGER participation_limiter
     FOR EACH ROW
     EXECUTE PROCEDURE verify_participation_limit();
 
-
-
 CREATE FUNCTION comment_from_participant() RETURNS TRIGGER AS $BODY$
 BEGIN
     IF NOT EXISTS (SELECT id_writer FROM participants WHERE NEW.id_event = participants.id_event AND comment.id_participant = NEW.id_writer) THEN
-        RAISE EXCEPTION 'COMMENT FROM NOT PARTICIPANT!'
+        RAISE EXCEPTION 'COMMENT FROM NOT PARTICIPANT!';
     END IF;
     RETURN NEW;
 END $BODY$
@@ -33,12 +26,10 @@ CREATE TRIGGER comment_restricter
     FOR EACH ROW
     EXECUTE PROCEDURE comment_from_participant();
 
-
-
 CREATE FUNCTION verify_participation_presence() RETURNS TRIGGER AS $BODY$
 BEGIN
     IF EXISTS (SELECT * FROM participants WHERE participants.id_event = NEW.id_event AND participants.id_participant = NEW.id_participant) THEN
-        RAISE EXCEPTION 'PARTICIPATION WAS ALREADY IN EVENT!'
+        RAISE EXCEPTION 'PARTICIPATION WAS ALREADY IN EVENT!';
     END IF;
 END $BODY$
 LANGUAGE plpgsql;
@@ -47,8 +38,6 @@ CREATE TRIGGER participation_presence
     BEFORE INSERT ON participants
     FOR EACH ROW 
     EXECUTE PROCEDURE verify_participation_presence();
-
-
 
 CREATE FUNCTION delete_likes() RETURNS TRIGGER AS $BODY$
 BEGIN
@@ -60,8 +49,6 @@ CREATE TRIGGER like_cleaning
     BEFORE INSERT ON likes
     FOR EACH ROW 
     EXECUTE PROCEDURE delete_likes();
-
-
 
 CREATE FUNCTION filter_votes() RETURNS TRIGGER AS $BODY$
 BEGIN 
