2ca34f74595801bdb9cecec52cc7fd30c901f24a
LeM0n17 <93678059+LeM0n17@users.noreply.github.com>
2023-11-18 21:44:40 +0000

    added some examples to the database and created models and controllers

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Events;
+use App\Models\UserJammer;
+
+class EventController extends Controller
+{
+    /**
+     * Show the card for a given id.
+     */
+    public function show(string $id): View
+    {
+        // Get the card.
+        $event = Events::findOrFail($id);
+
+        // Check if the current user can see (show) the card.
+        $this->authorize('show', $event);  
+
+        // Use the pages.card template to display the card.
+        return view('pages.event', [
+            'event' => $event
+        ]);
+    }
+
+    /**
+     * Shows all cards.
+     */
+    public function list(string $userId){
+        // Find the user by ID.
+        $user = UserJammer::findOrFail($userId);
+
+        // Check if the current user can list the events.
+        $this->authorize('list', Events::class);
+
+        // Retrieve events for the user ordered by ID.
+        $events = Auth::user()->events()->orderBy('id')->get();
+
+        // The current user is authorized to list events.
+
+        // Use the pages.events template to display all events.
+        return view('pages.events', [
+            'events' => $events
+        ]);
+    }
+
+    /**
+     * Creates a new card.
+     */
+    public function create(Request $request)
+    {
+        // Create a blank new Card.
+        $event = new Events();
+
+        // Check if the current user is authorized to create this card.
+        $this->authorize('create', $event);
+
+        // Set card details.
+        $event->name = $request->input('name');
+        $event->id_host = Auth::user()->id;
+
+        // Save the card and return it as JSON.
+        $event->save();
+        return response()->json($event);
+    }
+
+    /**
+     * Delete a card.
+     */
+    public function delete(Request $request, $id)
+    {
+        // Find the card.
+        $event = Events::find($id);
+
+        // Check if the current user is authorized to delete this card.
+        $this->authorize('delete', $event);
+
+        // Delete the card and return it as JSON.
+        $event->delete();
+        return response()->json($event);
+    }
+}
