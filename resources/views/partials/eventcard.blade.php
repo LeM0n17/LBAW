587568f9d1b8693cf207bc65b8db3cd0f3f285d7
LeM0n17 <93678059+LeM0n17@users.noreply.github.com>
2023-11-19 15:18:33 +0000

    added name of host to events

--- resources/views/partials/eventcard.blade.php
+++ resources/views/partials/eventcard.blade.php
@@ -1,6 +1,6 @@
 <div class="eventcard" id="{{ $event->id }}">
     <h3><a href="/events/{{ $event->id }}">{{ $event->name }}</a></h3>
-    <label>By {{ $event->user->name }}</label>
+    <label>By {{ $event->host->name }}</label>
     <label>{{ $event->start }} - {{ $event->end_ }}</label>
     <button type="button"> Request to Join </button>
 </div>
