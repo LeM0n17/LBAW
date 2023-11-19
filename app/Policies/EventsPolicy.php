7596305c34a02ea31b8019489943cb40ca8be850
tomas-sucena <tomas21sucena@gmail.com>
2023-11-19 13:22:40 +0000

    Update events policy
    Replace the class which represents the user.

--- app/Policies/EventsPolicy.php
+++ app/Policies/EventsPolicy.php
@@ -2,8 +2,7 @@
 
 namespace App\Policies;
 
-use App\Models\UserJammer;
-use App\Models\Users;
+use App\Models\User;
 use App\Models\Events;
 
 use Illuminate\Support\Facades\Auth;
@@ -21,7 +20,7 @@ class EventsPolicy
     /**
      * Determine if a given card can be shown to a user.
      */
-    public function show(UserJammer $user, Events $event): bool
+    public function show(User $user, Events $event): bool
     {
         // Only a card owner can see a card.
         return $user->id === $event->id_host;
@@ -39,7 +38,7 @@ class EventsPolicy
     /**
      * Determine if a card can be created by a user.
      */
-    public function create(UserJammer $user): bool
+    public function create(User $user): bool
     {
         // Any user can create a new card.
         return Auth::check();
@@ -48,7 +47,7 @@ class EventsPolicy
     /**
      * Determine if a card can be deleted by a user.
      */
-    public function delete(UserJammer $user, Events $event): bool
+    public function delete(User $user, Events $event): bool
     {
       // Only a card owner can delete it.
       return $user->id === $event->id_host;
