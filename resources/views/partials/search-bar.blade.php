baa866ff75e223a3faf459de7b54308d66ed89ce
tomas-sucena <tomas21sucena@gmail.com>
2023-11-20 19:25:18 +0000

    Fix search bar
    Remove @csrf.

--- resources/views/partials/search-bar.blade.php
+++ resources/views/partials/search-bar.blade.php
@@ -1,6 +1,5 @@
 <div id="search-bar">
     <form method="get" action="{{ route('search') }}">
-        @csrf
         <input type="text" placeholder="Search..." name="search" value="{{ Request::get('search') }}">
         <button type="submit">Search</button>
     </form>
