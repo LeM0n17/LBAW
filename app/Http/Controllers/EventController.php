cabe33959cbf0bef934f25f300c8707bb7a6c838
tomas-sucena <tomas21sucena@gmail.com>
2023-11-20 15:46:32 +0000

    Update event controller
    Remove unused parameter in editEvents().

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -154,7 +154,7 @@ class EventController extends Controller
         return view('pages.search', ['events' => $events]);
     }
 
-    public function editEvents(Request $request, $id)
+    public function editEvents(Request $request)
     {
         // Find the card.
         $id = $request->route('id');
