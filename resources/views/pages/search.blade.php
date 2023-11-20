b06af9d351ac13395f8599d6f154b104d7f53d09
tomas-sucena <tomas21sucena@gmail.com>
2023-11-20 00:30:44 +0000

    Delete unused database seed

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <link href="{{ url('css/home.css') }}" rel="stylesheet">
 <div id="search-bar">
-        <input type="text" placeholder="Search..." id="search-field">
-        <button type="submit" onclick="location.href='/search/'+ document.getElementById('search-field').value">Search</button>
+    <input type="text" placeholder="Search..." id="search-field">
+    <button type="submit" onclick="location.href='/search/'+ document.getElementById('search-field').value">Search</button>
 </div>
 <section id="featured" class="homesection">
     <h2 class="title">Featured Events</h2>
