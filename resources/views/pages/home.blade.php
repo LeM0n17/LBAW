f93f2e7f45c3e6ff994543598f8a74bdbd5a62ff
PBeGood4 <pedrocbeirao@gmail.com>
2023-12-12 11:04:36 +0000

    Dont show events status label if there is none

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,26 +4,32 @@
     <link href="{{ url('css/home.css') }}" rel="stylesheet">
     @include('partials.search-bar')
     <div id="searchResults">
-        <section id="featured" class="homesection">
-            <h2 class="title">Running Events</h2>
-            <div class="sidescroller">
-                @each('partials.eventcard', $running_events, 'event')
-            </div>
-        </section>
+        @if(count($running_events) > 0)
+            <section id="featured" class="homesection">
+                <h2 class="title">Running Events</h2>
+                <div class="sidescroller">
+                    @each('partials.eventcard', $running_events, 'event')
+                </div>
+            </section>
+        @endif
 
-        <section id="featured" class="homesection">
-            <h2 class="title">Upcoming Events</h2>
-            <div class="sidescroller">
-                @each('partials.eventcard', $upcoming_events, 'event')
-            </div>
-        </section>
+        @if(count($upcoming_events) > 0)
+            <section id="featured" class="homesection">
+                <h2 class="title">Upcoming Events</h2>
+                <div class="sidescroller">
+                    @each('partials.eventcard', $upcoming_events, 'event')
+                </div>
+            </section>
+        @endif
 
-        <section id="featured" class="homesection">
-            <h2 class="title">Finished Events</h2>
-            <div class="sidescroller">
-                @each('partials.eventcard', $finished_events, 'event')
-            </div>
-        </section>
+        @if(count($finished_events) > 0)
+            <section id="featured" class="homesection">
+                <h2 class="title">Finished Events</h2>
+                <div class="sidescroller">
+                    @each('partials.eventcard', $finished_events, 'event')
+                </div>
+            </section>
+        @endif
     </div>
 
     <a href="/createevents"><button id="createevent">+</button></a>
