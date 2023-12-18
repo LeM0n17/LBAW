92fefc87b3124a36dc1d39f4971f9751f142e881
tomas-sucena <tomas21sucena@gmail.com>
2023-12-18 23:53:55 +0000

    Add email template

---
+++ resources/views/emails/email.blade.php
@@ -0,0 +1,9 @@
+Hi {{ $mailData['name'] }},<br>
+<br>
+Forgot your password?<br>
+We received a request to reset the password for your account.<br>
+<br>
+To reset your password, click on the button below.<br>
+<br>
+Or copy and paste the URL into your browser:<br>
+{{ $mailData['name'] }}.<br>
