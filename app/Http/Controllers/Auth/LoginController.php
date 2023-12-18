a7e7ed84dec46074f31ef34dfe3b6770b52af8a0
tomas-sucena <tomas21sucena@gmail.com>
2023-12-18 17:35:53 +0000

    Update login controller
    Add functions necessary for recovering passwords.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class LoginController extends Controller
@@ -62,5 +64,59 @@ class LoginController extends Controller
         $request->session()->regenerateToken();
         return redirect()->route('login')
             ->withSuccess('You have logged out successfully!');
-    } 
+    }
+
+    /**
+     * Recover password.
+     */
+    public function recoverPassword(Request $request) {
+
+        $user = User::where('email', '=', $request->recoverAttemp)->first();
+        if (!$user) return redirect()->route('login')->with('error', "Invalid email");
+
+        if (Hash::check($request->recoverToken, $user->password)) {
+            // verify if the passwords match
+            if($request->recoverPassword1 != $request->recoverPassword2)
+                return redirect()->route('login')->with('match_error', "Passwords don't match")
+                ->with('email_attemp', $request->recoverAttemp);
+
+            // verify if the password has the right length
+            if(strlen($request->recoverPassword1) < 8)
+                return redirect()->route('login')->with('size_error', "Password must have at least 8 characters")
+                ->with('email_attemp', $request->recoverAttemp);
+
+            $user->password = bcrypt($request->recoverPassword1);
+            $user->save();
+
+            return redirect()->route('login')->with('success', "Your password has been changed successfully");
+        }
+        return redirect()->route('login')->with('invalid_token', "Invalid token. Please try again.")
+            ->with('email_attemp', $request->recoverAttemp);
+    }
+
+    /**
+     * Sends an email
+     */
+    public function sendEmail(Request $request) {
+        $user = User::where('email', '=', $request->email)->first();
+
+        // verify if the user exists
+        if (!$user)
+            return;
+
+        // generate a random token
+        $token = Str::random();
+
+        $data = array('name' => $user->name,
+            'token' => $token);
+
+        Mail::send('partials.mail', $data, function($message) {
+            $message->subject('Recover your password!');
+            $message->from('OnlyFEUP@gmail.com','OnlyFEUP');
+            $message->to('user@gmail.com', 'OnlyFEUP User');
+        });
+
+        $user->password = bcrypt($token);
+        $user->save();
+    }
 }
