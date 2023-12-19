2fc942573073a396278b8ca0fc909615ac0d5fbb
tomas-sucena <tomas21sucena@gmail.com>
2023-12-19 13:50:48 +0000

    Fix mail controller

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -14,26 +14,24 @@ class MailController extends Controller
      * Sends an email requesting the user to reset their password.
      */
     public function sendPasswordRecoveryEmail(Request $request) {
-        $user = User::where('email', '=', $request->email)->get();
+        $username = User::where('email', $request->email)->value('name');
 
         // verify if the user exists
-        if ($user == NULL)
-            return redirect()->route('home')
-                ->with('error', 'That email does not belong to any user!');
+        if ($username == NULL)
+            return redirect()->route('showRecoverPasswordForm')
+                ->withErrors('error', 'That email does not belong to any user!');
 
         // generate a random token
         $token = Str::random();
 
         $mailData = array(
-            'name' => $user->name,
+            'name' => $username,
+            'email' => $request->email,
             'token' => $token,
         );
 
         Mail::to($request->email)->send(new MailModel($mailData));
-
-        $user->password = bcrypt($token);
-        $user->save();
-
-        return redirect()->route('home');
+        return redirect()->route('login')
+                ->withSuccess('A password reset link has been sent to your email!');
     }
 }
--- app/Mail/MailModel.php
+++ app/Mail/MailModel.php
@@ -42,7 +42,7 @@ class MailModel extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.recover-password',
         );
     }
 
