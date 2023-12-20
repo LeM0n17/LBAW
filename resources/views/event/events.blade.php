4bc1e8a8701aa628dcd21f022f02351b8c6738e4
Afoba <96028158+Afoba@users.noreply.github.com>
2023-12-20 15:07:21 +0000

    Attempted to fix merging issues

--- resources/views/event/events.blade.php
+++ resources/views/event/events.blade.php
@@ -9,22 +9,15 @@
     <div class="tagcontainer">
         @each('partials.tagdisplay', $event->tags, 'tag')
     </div>
-    @if ($event->participants->contains('id_participant', Auth::user()->id))
-        <label id="userin">You are already registered in this event!</label>
-    @elseif ($event->notifications->contains('id_developer', Auth::user()->id))
-        <form method="POST" action="{{ route('addHomeParticipant', ['id' => $event->id]) }}" id="joinform">
-            {{ csrf_field() }}
-            <button type="submit"> Accept Invite </button>
-        </form>
-    @endif
+
     @if (Auth::user()->id == $event->host->id)
         <div class="buttoncontainer">
-            <button type="button"><a href="/editevents/{{ $event->id }}"> Configure </a></button>
             <button type="button"><a href="/tagconfig/{{ $event->id }}"> Tags </a></button>
+            <button type="button"><a href="/editevents/{{ $event->id }}"> Configure </a></button>
             <button type="button"><a href="/participants/{{ $event->id }}"> Participants </a></button>
-            <form method="POST" action="{{ route('deleteevents', ['id' => $event->id]) }}" id="deleteform">
+            <form method="POST" action="{{ route('deleteevents', ['id' => $event->id]) }}">
                 {{ csrf_field() }}
-                <button type="submit" id="deletebutton"> Delete </button>
+                <button type="submit"> Delete </button>
             </form>
             <form method="POST" action="{{ route('cancelevent', ['event_id' => $event->id]) }}">
                 {{ csrf_field() }}
@@ -32,7 +25,29 @@
             </form>
         </div>
     @endif
-    <hr>
+    @if ($event->notifications->where('type', 'invite')->contains('id_developer', Auth::user()->id))
+        <form method="POST" action="{{ route('addHomeParticipant', ['id' => $event->id]) }}">
+            {{ csrf_field() }}
+            <button type="submit"> Accept Invite </button>
+        </form>
+    @endif
+    @if (!$event->participants->contains('id_participant', Auth::user()->id))
+        @if (!$event->notifications->where('type', 'request')->contains('id_developer', Auth::user()->id))
+            <form method="POST" action="{{ route('requestToJoin', ['event_id' => $event->id, 'user_id' => Auth::user()->id]) }}">
+                {{ csrf_field() }}
+                <button type="submit"> Request to Join </button>
+            </form>
+        @else
+            <p id="request">Request Sent waiting on answer from host</p>
+        @endif
+    @endif
+    @if($event->participants->contains('id_participant', Auth::user()->id))
+        <form method="POST" action="{{ route('leaveEvent', ['id' => $event->id]) }}">
+            {{ csrf_field() }}
+            @method('DELETE')
+            <button type="submit"> Leave Event </button>
+        </form>
+    @endif
     <p id="description">{{ $event->description }}</p>
 </div>
 <div class="commentsection">
@@ -55,3 +70,4 @@
         <button type="submit">Add Comment</button>
     </form>
 </div>
+@endsection
