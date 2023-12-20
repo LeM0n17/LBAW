743685c57c302aaa8d13d86b7f58f39041552e1d
PBeGood4 <pedrocbeirao@gmail.com>
2023-12-20 01:10:47 +0000

    Edit profile picture

--- database/jammer-seed.sql
+++ database/jammer-seed.sql
@@ -300,7 +300,7 @@ COMMIT;
 -- POPULATE
 -----------------------------------------
 
-INSERT INTO users (id, name, password, email, image) VALUES (DEFAULT, 'JohnDoe', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'example@example.com', 'pfps/1.png');
+INSERT INTO users VALUES (DEFAULT, 'JohnDoe', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'example@example.com');
 INSERT INTO users VALUES (DEFAULT, 'JaneDoe', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'example1@example.com');
 INSERT INTO users VALUES (DEFAULT, 'JorgeDoe', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'example2@example.com');
 
--- resources/views/profile/editprofile.blade.php
+++ resources/views/profile/editprofile.blade.php
@@ -4,14 +4,22 @@
     <link href="{{ url('css/profile.css') }}" rel="stylesheet">
     
     <div class="main">
-        <form method="POST" action="{{ route('editprofile') }}">
+        <form method="POST" action="{{ route('editprofile') }}" enctype="multipart/form-data">
         {{ csrf_field() }}
             <div>
                 <a class="button" href="{{ url('/profile') }}">Cancel</a>
                 <button type="submit">Save</button>
             </div>
             <br>
-            <div class="fa-regular fa-user fa-2xl profile-pic"></div>
+                @if (Auth::user()->image == "")
+                    <img src="{{URL::asset('/images/default_pfp.png')}}" height="150" width="150" style="padding: none;">
+                @else
+                    <img src="{{URL::asset('storage/'.Auth::user()->image)}}" height="150" width="150" style="padding: none;">
+                @endif
+                <br>
+                <label for="image">Profile Picture:</label>
+                <input id="image" type="file" enctype="multipart/form-data" name="image" src="" alt="Submit Image" accept="image/png, image/jpeg" width="100%" height="48">
+                <br>
                 <label for="name">Username:</label>
                 <input type="text" id="name" name="name" value="{{ Auth::user()->name }}"><br><br>
                 <label for="email">Email:</label>
