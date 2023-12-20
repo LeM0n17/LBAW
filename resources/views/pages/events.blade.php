9dc8801738438487e5d299eb5665b3f6cc38e3d1
Afoba <96028158+Afoba@users.noreply.github.com>
2023-12-20 15:05:06 +0000

    Attempted to fix merging issues

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -4,20 +4,27 @@
 <link href="{{ url('css/event.css') }}" rel="stylesheet">
 <div class="eventinfo">
     <h2 id = "title">{{ $event->name }}</h2>
-    @if (Auth::user()->id == $event->host->id)
-        <button type="button"><a href="/editevents/{{ $event->id }}"> Configure </a></button><br>
-        <button type="button"><a href="/participants/{{ $event->id }}"> Participants </a></button><br>
-        <form method="POST" action="{{ route('deleteevents', ['id' => $event->id]) }}">
-            {{ csrf_field() }}
-            <button type="submit"> Delete </button>
-        </form>
-        <form method="POST" action="{{ route('cancelevent', ['event_id' => $event->id]) }}">
-            {{ csrf_field() }}
-            <button type="submit"> Cancel </button>
-        </form>
-    @endif
     <label id="creator">By <b>{{ $event->host->name }}</b></label>
     <label id="duration">{{ $event->start }} - {{ $event->end_ }}</label>
+    <div class="tagcontainer">
+        @each('partials.tagdisplay', $event->tags, 'tag')
+    </div>
+
+    @if (Auth::user()->id == $event->host->id)
+        <div class="buttoncontainer">
+            <button type="button"><a href="/tagconfig/{{ $event->id }}"> Tags </a></button>
+            <button type="button"><a href="/editevents/{{ $event->id }}"> Configure </a></button>
+            <button type="button"><a href="/participants/{{ $event->id }}"> Participants </a></button>
+            <form method="POST" action="{{ route('deleteevents', ['id' => $event->id]) }}">
+                {{ csrf_field() }}
+                <button type="submit"> Delete </button>
+            </form>
+            <form method="POST" action="{{ route('cancelevent', ['event_id' => $event->id]) }}">
+                {{ csrf_field() }}
+                <button type="submit"> Cancel </button>
+            </form>
+        </div>
+    @endif
     @if ($event->notifications->where('type', 'invite')->contains('id_developer', Auth::user()->id))
         <form method="POST" action="{{ route('addHomeParticipant', ['id' => $event->id]) }}">
             {{ csrf_field() }}
