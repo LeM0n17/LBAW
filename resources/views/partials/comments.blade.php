45f3e891a6eb4e625cb85182e00f2880a206cc28
Afoba <96028158+Afoba@users.noreply.github.com>
2023-12-21 21:56:24 +0000

    Added comment css

--- resources/views/partials/comments.blade.php
+++ resources/views/partials/comments.blade.php
@@ -1,14 +1,15 @@
-<div>
-    <h4>{{ $comment->user->name }}</h4>
-    <p>{{ $comment->content }}</p>
-    <p>{{ $comment->time }}</p>
+<div class="comment">
+    <h4 id="user">{{ $comment->user->name }}</h4>
+    <p id="content">{{ $comment->content }}</p>
+    <p id="time">{{ $comment->time }}</p>
     @if(Auth::id() == $comment->id_writer || Auth::id() == $comment->event->id_host)
-        <form method="POST" action="{{ route('deletecomment', ['id' => $comment->id]) }}">
+        <form method="POST" action="{{ route('deletecomment', ['id' => $comment->id]) }}" id="deletecom">
             {{ csrf_field() }}
             @method('DELETE')
             <button type="submit"> Delete </button>
         </form>
     @endif
+    <div class="sidecontainer">
     <p>{{ $comment->likesCount() }} likes</p>
     @if($comment->isLikedBy(Auth::user()))
         <form method="POST" action="{{route('removeLike', ['id_comment' => $comment->id])}}">
@@ -22,7 +23,6 @@
             <button type="submit" style="color:blue">&#x25B2;</button>
         </form>
     @endif
-    <p>{{ $comment->dislikesCount() }} dislikes</p>
     @if($comment->isDislikedBy(Auth::user()))
         <form method="POST" action="{{route('removeLike', ['id_comment' => $comment->id])}}">
             {{ csrf_field() }}
@@ -35,4 +35,6 @@
             <button type="submit" style="color:blue">&#x25BC;</button>
         </form>
     @endif
+    <p>{{ $comment->dislikesCount() }} dislikes</p>
+    </div>
 </div>
