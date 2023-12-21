e2d57fb6c2f3169b7d36ca7030c5e9632fd5112c
Afoba <96028158+Afoba@users.noreply.github.com>
2023-12-21 22:19:45 +0000

    Fixed small problematic condition

--- resources/views/partials/participant.blade.php
+++ resources/views/partials/participant.blade.php
@@ -1,4 +1,4 @@
-@if (!Auth::user()->id == $participant->id)
+@if (!Auth::user()->id == $participant->participant->id)
 <div class="participant" id="{{ $participant->id }}">
     <label id="username"><strong>{{ $participant->participant->name }}</strong></label>
     <form method="POST" action="{{ route('removeParticipant', ['id_participant' => $participant->id_participant]) }}">
