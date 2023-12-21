924aab427dde0d849100b0e61b1cdbcaca5ee871
LeM0n17 <93678059+LeM0n17@users.noreply.github.com>
2023-12-21 22:32:47 +0000

    fixed the css for the notifications

--- resources/views/partials/invitationnotification.blade.php
+++ resources/views/partials/invitationnotification.blade.php
@@ -1,4 +1,4 @@
-<div class="eventcard" id="{{ $notification->id }}">
+<div class="notification" id="{{ $notification->id }}">
     <h3><a href="/events/{{ $notification->id_event }}">{{ $notification->event->name }}</a></h3>
     <p>Invitation sent to {{ $notification->developer->name}}</p>
     <form method="POST" action="{{ route('refuseParticipantFromRequest', ['id_notification' => $notification->id]) }}">
