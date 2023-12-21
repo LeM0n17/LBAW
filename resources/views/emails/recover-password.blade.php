440285b48de4a2162701513696cc9636351d917f
tomas-sucena <tomas21sucena@gmail.com>
2023-12-21 23:14:25 +0000

    Update recover password email

--- resources/views/emails/recover-password.blade.php
+++ resources/views/emails/recover-password.blade.php
@@ -1,11 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<link href="{{url('css/login.css')}}" rel="stylesheet">
 Hi {{ $mailData['name'] }},<br>
 <br>
 Forgot your password?<br>
 We received a request to reset the password for your account.<br>
 <br>
 To reset your password, click on the button below.<br>
-<br>
-Or click <a href="{{ route('resetPassword', ['token' => $mailData['token'], 'email' => $mailData['email'] ]) }}">here</a>.<br>
+<a class="button button-outline" href="{{ route('resetPassword', ['token' => $mailData['token'], 'email' => $mailData['email'] ]) }}">Reset password</a>
 <br>
 Best regards,<br>
 The Jammer Team
+@endsection
